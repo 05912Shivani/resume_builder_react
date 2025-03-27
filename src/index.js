6029e331e5e1675857ce376930f5d7de0ddfb6ea
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-03-27 21:38:32 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
-
+// Getting the root container element from the HTML file
 const container = document.getElementById('root');
+// Creating a root using React 18's createRoot method
 const root = createRoot(container); 
 
+// Rendering the App component inside Redux Provider to provide access to the store
 root.render(
-  <Provider store={store}>
-    <App />
+  <Provider store={store}>{/* Wrapping the app with Provider to pass down the Redux store */}
+    <App />{/* Main application component */}
   </Provider>
 );
 
