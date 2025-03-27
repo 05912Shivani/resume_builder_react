387672a44baf91eb714fe493587a891d406e068a
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-03-27 21:35:16 +0530

    store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,8 @@ import formDataReducer from './formDataSlice';
 
 export const store = configureStore({
   reducer: {
-    templates: templatesReducer,
-    formData: formDataReducer,
+    templates: templatesReducer, // Assigning templates slice reducer to "templates" state
+    formData: formDataReducer,// Assigning formData slice reducer to "formData" state
   },
 });
 
