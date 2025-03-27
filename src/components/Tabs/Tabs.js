6f49109265e8f9a067484cb65ab5957f23ab49d5
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-03-27 21:08:43 +0530

    Tabs.js

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,14 +1,24 @@
-// import React from 'react';
-// import { Tabs, Tab } from '@mui/material';
+/**
+ * TabNavigation Component
+ * Provides tab-based navigation using Material-UI's Tabs and Tab components.
+ * 
+ * Props:
+ * - value (Number): The currently selected tab index.
+ * - onChange (Function): Callback function triggered when a tab is selected.
+ * - tabs (Array): An array of objects containing tab labels.
+ */
 
-// const TabNavigation = ({ value, onChange, tabs }) => {
-//   return (
-//     <Tabs value={value} onChange={onChange} aria-label="Details Tabs" centered>
-//       {tabs.map((tab, index) => (
-//         <Tab key={index} label={tab.label} />
-//       ))}
-//     </Tabs>
-//   );
-// };
+import React from 'react';
+import { Tabs, Tab } from '@mui/material';
 
-// export default TabNavigation;
+const TabNavigation = ({ value, onChange, tabs }) => {
+  return (
+    <Tabs value={value} onChange={onChange} aria-label="Details Tabs" centered>
+      {tabs.map((tab, index) => (
+        <Tab key={index} label={tab.label} />
+      ))}
+    </Tabs>
+  );
+};
+
+export default TabNavigation;
