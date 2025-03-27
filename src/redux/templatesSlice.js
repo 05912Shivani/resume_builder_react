7a44a121c06eec1a78b7448f740904d00292dacf
Shivani Sharma <92907798+05912Shivani@users.noreply.github.com>
2025-03-27 21:35:59 +0530

    templatesSlice.js

--- src/redux/templatesSlice.js
+++ src/redux/templatesSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 import templatesData from '../utils/Data';
 const templatesSlice = createSlice({
-  name: 'templates',
-  initialState: templatesData,
-  reducers: {},
+  name: 'templates', // Slice name used in Redux state
+  initialState: templatesData,// Setting initial state with predefined template data
+  reducers: {},// No reducers are defined as this slice is read-only for now
 });
 
 
